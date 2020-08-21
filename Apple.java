7117516e9c30db37d2f024707fdaa325ee929996
Anna-прог-20 <anutka.love@mail.ru>
2020-08-21 13:58:16 +0900

    Убрала не нужные параметры

--- Apple.java
+++ Apple.java
@@ -1,10 +1,8 @@
 package com.company;
 
 public class Apple extends Fruit{
-    private double weight=1.0f;
 
-    @Override
-    public double getWeight() {
-        return weight;
+    public Apple(double weight) {
+        super(weight);
     }
 }
--- Orange.java
+++ Orange.java
@@ -1,10 +1,8 @@
 package com.company;
 
 public class Orange extends Fruit{
-    private double weight=1.5f;
 
-    @Override
-    public double getWeight() {
-        return weight;
+    public Orange(double weight) {
+        super(weight);
     }
 }
