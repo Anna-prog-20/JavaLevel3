db1bec9dd5374758d1c37325c5473b82fdb121d9
Anna-прог-20 <anutka.love@mail.ru>
2020-08-26 12:28:55 +0900

    Реалиция аутентификации через БД

--- BaseAuthService.java
+++ BaseAuthService.java
@@ -1,45 +1,15 @@
 package com;
 
-import com.mysql.jdbc.Driver;
-
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class BaseAuthService implements AuthService{
-    private List<Entry> entries;
+    private Connection connection;
 
-    public BaseAuthService() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException var7) {
-            throw new RuntimeException("Driver not found");
-        }
-
-        Connection connection;
-        try {
-            DriverManager.registerDriver(new Driver());
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CHAT", "root", "");
-        } catch (SQLException var6) {
-            var6.printStackTrace();
-            throw new RuntimeException("Driver Registration error");
-        }
-
-        try {
-            entries=new ArrayList<>();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT login, password, nickname FROM USERS");
-            while(true) {
-                if (!resultSet.next()) {
-                    System.out.println(entries);
-                    break;
-                }
-                entries.add(new Entry(resultSet.getString("login"), resultSet.getString("password"), resultSet.getString("nickname")));
-            }
-        } catch (SQLException var8) {
-            var8.printStackTrace();
-            throw new RuntimeException("Statement error");
-        }
+    public BaseAuthService(Connection connection) {
+        this.connection = connection;
     }
 
     @Override
@@ -54,35 +24,20 @@ public class BaseAuthService implements AuthService{
 
     @Override
     public String getNickByLoginAndPass(String login, String password) {
-        for (Entry entry : entries) {
-            if (entry.getLogin().equals(login) && entry.getPassword().equals(password)) {
-                return entry.getNickname();
-            }
+        try {
+            ResultSet clients = questClient(login,password);
+            if(clients.next())
+                return clients.getString("nickname");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
         return null;
     }
 
-    private class Entry {
-        private String login;
-        private String password;
-        private String nickname;
-
-        public Entry(String login, String password, String nickname) {
-            this.login = login;
-            this.password = password;
-            this.nickname = nickname;
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public String getNickname() {
-            return nickname;
-        }
+    private ResultSet questClient(String login, String password) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM USERS WHERE login=? and password=?");
+        preparedStatement.setString(1,login);
+        preparedStatement.setString(2,password);
+        return preparedStatement.executeQuery();
     }
 }
