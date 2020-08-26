1220eb9240c82a7f39a77f1a10fcd0b1ba216f2b
Anna-прог-20 <anutka.love@mail.ru>
2020-08-26 12:30:19 +0900

    Добавление подключения к БД

--- Server.java
+++ Server.java
@@ -1,8 +1,11 @@
 package com;
 
+import com.mysql.jdbc.Driver;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +13,12 @@ public class Server {
     private final int port;
     private AuthService authService;
     private List<ClientHandler> clients;
+    private Connection connection;
 
     public Server(int port) {
         this.port = port;
         this.clients = new ArrayList<>();
+        connectionBD();
         start();
     }
 
@@ -22,8 +27,9 @@ public class Server {
     }
 
     private void start() {
+
         try (ServerSocket server = new ServerSocket(this.port)) {
-            authService = new BaseAuthService();
+            authService = new BaseAuthService(connection);
             authService.start();
 
             while(true) {
@@ -31,7 +37,7 @@ public class Server {
                 System.out.println("Ожидаю подключения клиента...");
                 Socket socket = server.accept();
                 System.out.println(String.format("Клиент подключился: %s", socket.toString()));
-                ClientHandler clientHandler=new ClientHandler(this, socket);
+                new ClientHandler(this, socket);
             }
         } catch (IOException e) {
             System.out.println("Ошибка при запуске сервера.");
@@ -43,6 +49,22 @@ public class Server {
         }
     }
 
+    private void connectionBD(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException var7) {
+            throw new RuntimeException("Driver not found");
+        }
+
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CHAT", "root", "");
+        } catch (SQLException var6) {
+            var6.printStackTrace();
+            throw new RuntimeException("Driver Registration error");
+        }
+    }
+
     public synchronized boolean isNickBusy(String nickname) {
         for (ClientHandler clientHandler : clients) {
             if (clientHandler.getName().equals(nickname)) {
@@ -51,7 +73,6 @@ public class Server {
         }
         return false;
     }
-
     public synchronized boolean isNickFree(String nickname) {
         return !isNickBusy(nickname);
     }
@@ -61,32 +82,39 @@ public class Server {
             clientHandler.sendMessage(message);
         }
     }
-    public synchronized void broadcastClientsList() {
-        StringBuilder sb = new StringBuilder("/clients ");
-        for (ClientHandler clientHandler : clients) {
-            sb.append(clientHandler.getName() + " ");
-        }
-        broadcastMessage(sb.toString());
-    }
+//    public synchronized void broadcastClientsList() {
+//        StringBuilder sb = new StringBuilder("/clients ");
+//        while (true) {
+//            try {
+//                if (!clientsOnline.next()) break;
+//                sb.append(clientHandler.getName() + " ");
+//            } catch (SQLException throwables) {
+//                throwables.printStackTrace();
+//            }
+////            for (ClientHandler clientHandler : clients) {
+////                sb.append(clientHandler.getName() + " ");
+////            }
+//        }
+//        broadcastMessage(sb.toString());
+//    }
 
     public synchronized void sendMessageToClient(ClientHandler from, String nickTo, String msg) {
-        for (ClientHandler clientHandler : clients) {
-            if (clientHandler.getName().equals(nickTo)) {
-                clientHandler.sendMessage("от " + from.getName() + ": " + msg);
-                from.sendMessage("клиенту " + nickTo + ": " + msg);
-                return;
-            }
+       for (ClientHandler clientHandler : clients) {
+           if (clientHandler.getName().equals(nickTo)) {
+            clientHandler.sendMessage("от " + from.getName() + ": " + msg);
+            from.sendMessage("клиенту " + nickTo + ": " + msg);
+            return;
+           }
         }
         from.sendMessage("Участника с ником " + nickTo + " нет в чате");
     }
 
 
     public synchronized void subscribe(ClientHandler clientHandler) {
-        clients.add(clientHandler);
+       clients.add(clientHandler);
     }
 
     public synchronized void unsubscribe(ClientHandler clientHandler) {
-
         clients.remove(clientHandler);
     }
 }
